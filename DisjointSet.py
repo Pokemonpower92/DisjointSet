2d1dbbe0db01cb093c3ef16d2a708fa8bf928f02
Poochofpain <poochofpain92@gmail.com>
2020-04-25 16:19:33 -0400

    More fine tuning

--- DisjointSet.py
+++ DisjointSet.py
@@ -7,7 +7,7 @@ class Node:
 
     def __init__(self, data):
         self.data = str(data)
-        self.link = None
+        self.link = self
         self.size = 1
 
     def clear(self):
@@ -23,13 +23,35 @@ class DisjointSet:
         self.size = len(verticies)
         self.components = self.size
 
+    # Simply prints the set for bug testing.
     def print(self):
         for v in self.verticies.values():
-            print(v.data + ':', v.link)
+            print(v.data + ':', v.link.data)
         print("Number of connected components:", self.components)
 
+    # Finds the root of the node with the given data.
     def find(self, data):
-        return
+
+        try:
+            node = self.verticies[data]
+
+            path = []
+
+            while node.link != node:
+                path.append(node)
+                node = self.verticies[node.link.data]
+
+            for v in path:
+                v.link = node
+
+            return node.data
+
+        except KeyError:
+            print('Invalid Key')
+            return None
+
+
+
 
 links = {}
 
@@ -37,6 +59,18 @@ for x in range(10):
     n = Node(x)
     links[n.data] = n
 
+links['0'].link = links['2']
+links['1'].link = links['2']
+links['3'].link = links['1']
+links['4'].link = links['3']
+
 set = DisjointSet(links)
+
+set.print()
+
+b = set.find(4)
+
+if b:
+    print(b)
 
 set.print()
